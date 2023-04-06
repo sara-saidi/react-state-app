d753461260059bcd44a3a48e2af6636831e3af58
sara-saidi <saidisarra782022@gmail.com>
2023-04-06 14:01:47 +0100

    update

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react'
 import Profile from './Profile'
 
-import Counter from './Counter';
+//import Counter from './Counter';
 export default class AppClass extends Component {
   
   
@@ -12,18 +12,33 @@ export default class AppClass extends Component {
           name:"houyem",
           lastName:"yazidi",
           profession:"developer",
-          
-            show:false
+          count:0,
+            show:false,
+            date: new Date()
         }
     }
     
     show=()=>{
 this.setState({ show:!this.state.show})
     }
+    componentDidMount() {
+      this.timerID = setInterval(
+        () => this.tick(),
+        1000
+      );
+    }
+    componentWillUnmount() {
+      clearInterval(this.timerID);
+    }
+  
+    tick() {
+      this.setState({
+        date: new Date()
+      });}
   render() {
     return (
       <div style={{backgroundColor:'white' }}>
-       <Counter/>
+      <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
        {this.state.show? <Profile name={this.state.name} lastName={this.state.lastName} profession={this.state.profession} photo={this.state.photo}/>:null}
         <button onClick={this.show} style={{backgroundColor:'green' }}>show me </button>
       </div>
